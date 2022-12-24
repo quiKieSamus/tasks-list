1c9dbb9f54a232985c7e79644f6d5240abdca71a
= <gonzru01atwork@gmail.com>
2022-12-23 22:39:47 -0400

    pseudosessions

--- public/scripts/createTask.js
+++ public/scripts/createTask.js
@@ -6,64 +6,76 @@ const hostUrl = "http://localhost:8080";
 ////this can change, check your computer's ip
 const networkUrl = "http://192.168.0.189:8080";
 
-send.addEventListener('click', () => {
+fetch(`${hostUrl}/login/users`, { method: "POST" })
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        let user;
+        for (let i = 0; i < data.length; i++) {
+            if (location.href.search(data[i])) user = data[i];
+            else return;
+        }
+        console.log(user);
+        send.addEventListener('click', () => {
+            const title = document.querySelector('#title').value;
+            const desc = document.querySelector('#desc').value;
+            const starts = document.querySelector('#starts').value;
+            const ends = document.querySelector('#ends').value;
+            const urgencyTrue = document.querySelector('#yes').value;
+            const urgencyFalse = document.querySelector('#not').value;
+            let urgency = false;
+            if (!urgencyTrue) {
+                urgency = urgencyFalse
+            }
+            urgency = urgencyTrue;
 
-    const title = document.querySelector('#title').value;
-    const desc = document.querySelector('#desc').value;
-    const starts = document.querySelector('#starts').value;
-    const ends = document.querySelector('#ends').value;
-    const urgencyTrue = document.querySelector('#yes').value;
-    const urgencyFalse = document.querySelector('#not').value;
-    let urgency = false;
-    if (!urgencyTrue) {
-        urgency = urgencyFalse
-    }
-    urgency = urgencyTrue;
+            const data = {
+                title: title,
+                desc: desc,
+                starts: starts,
+                ends: ends,
+                urgency: urgency,
+            };
+            console.log(data);
 
-    const data = {
-        title: title,
-        desc: desc,
-        starts: starts,
-        ends: ends,
-        urgency: urgency
-    };
-    console.log(data);
-
-    const fetchParams = {
-        headers: {
-            "content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(data),
-        method: "POST"
-    };
+            const fetchParams = {
+                headers: {
+                    "content-type": "application/json; charset=UTF-8"
+                },
+                body: JSON.stringify(data),
+                method: "POST"
+            };
 
 
-    /*
-        fetch can be used to make http requests
-        in this case we do a post request to the server
-        sending data on the notes to add
-
-        in this case we are sending a json
-        it is important that the content-type header is set to 
-        "application/json; charset=UTF-8"
-        Since its a JSON the body of the request needs to have 
-        a JSON NOT A JS OBJECT (which is one of the reasons 
-        it took me so long to fix
-        And of course, for it to be a post request you gotta 
-        first set the method to post
+            /*
+                fetch can be used to make http requests
+                in this case we do a post request to the server
+                sending data on the notes to add
         
-        in this example we store all configurations in an object called fetchParams which, at the time of writing this commentary, is above)
-    */
-    fetch(`${hostUrl}/tasks/createTask`, fetchParams)
+                in this case we are sending a json
+                it is important that the content-type header is set to 
+                "application/json; charset=UTF-8"
+                Since its a JSON the body of the request needs to have 
+                a JSON NOT A JS OBJECT (which is one of the reasons 
+                it took me so long to fix
+                And of course, for it to be a post request you gotta 
+                first set the method to post
+                
+                in this example we store all configurations in an object called fetchParams which, at the time of writing this text, is above this comment)
+            */
 
-        .then((data) => {
+            fetch(`${hostUrl}/tasks/${user}/createTask`, fetchParams)
+                .then((resolve) => {
+                    return resolve.json();
+                })
+                .then((data) => {
+                    location.href = data.link;
+                })
+                .catch((err) => console.log(err));
 
-            return data.json();
-
-        })
-        .catch((err) => console.log(err));
-
-    setTimeout(() => {
-        location.href = `${hostUrl}/tasks`;
-    }, 1000);
-})
+            // setTimeout(() => {
+            //     location.href = `${hostUrl}/tasks`;
+            // }, 1000);
+        });
+    });
